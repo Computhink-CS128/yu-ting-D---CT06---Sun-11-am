d6427654a0200a903ec2dac8d90ea92abf16eb5f
Computhink-CS128 <cs128@Computhink.com.sg>
2025-07-20 04:35:05 +0000

    Co-authored-by: Lyn101s <Lyn101s@users.noreply.github.com>

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -1,15 +1,26 @@
 /*
-Task 1: Add the setup() function
+Task 1: Add the setup() 
 */
+function setup() {
+  createCanvas(600 , 400);
+ background(220) //red,green,blue
+}
 
 function draw() {
   // Challenge 1: An eye using ellipse() function
+// ellipse(100,100,80,50);
+// ellipse(182,100,80,50);
 
-  // Challenge 2: A face using circle() function
+//Challenge 2: A face using circle() function
+// circle(300,200,150);
+// circle(300,200,100);
 
   // Challenge 3: A house using rect() function
+// rect(100,100,100,200,20,10,30,25);
 
   // Challenge 4: A rocketship using triangle() function
+triangle(30,75,58,20,86,75);
+triangle(30,45,75,40,57,32);
 
   // Challenge 5: A square face using quad() function
 
