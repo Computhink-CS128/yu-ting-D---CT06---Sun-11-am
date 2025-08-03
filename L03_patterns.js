a7c941a3023c2f617df94f5c9fde44bdaaa4893c
Computhink-CS128 <cs128@Computhink.com.sg>
2025-08-03 03:53:59 +0000

    Aug 3, 2025, 3:53 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -1,47 +1,79 @@
-let spacing = 50;
-//let colorValue = 0;
+// let spacing = 50;
+// //let colorValue = 0;
+
+// function setup() {
+//     createCanvas(600, 400);
+//     background(220);
+//     // noLoop(); // Stops continuous drawing
+//   }
+  
+// function draw() {
+//   // Recap 1: Repeating Circles
+// //   fill(0,0,255)
+// // circle(75,200,100);
+// // circle(225,200,100);
+// // circle(375,200,100);
+// // circle(525,200,100);
+
+// // for (let i = 0; i<10; i++){
+// //   // fill(0,0,i * spacing)
+// //   // circle(spacing + spacing*50,200,40);
+// //   fill(colorValue%255);
+// //   circle(spacing + i*spacing,200,40);
+// //   colorValue += 1;
+
+
+//   // Task 1: Colour Gradient
+
+//   // Task 2: Colour Loop
+// for (let i = 0; i<10; i++){
+//   if(i%2 === 0){
+//     fill(0);
+//   }
+//   else {
+//     fill(255);
+//   }
+// }
+//   circle(spacing + i*spacing,200,40);
+//   rect(spacing + i*spacing,250,50,50);
+
+
+
+//   // Task 3: Row of Circles
+
+//   // Task 4: Grid of Circles
+
+
+// }
+
+let spacing = 80; 
+// let colorValue = 0;
 
 function setup() {
-    createCanvas(600, 400);
+    createCanvas(600, 600);
     background(220);
-    // noLoop(); // Stops continuous drawing
-  }
-  
-function draw() {
-  // Recap 1: Repeating Circles
-//   fill(0,0,255)
-// circle(75,200,100);
-// circle(225,200,100);
-// circle(375,200,100);
-// circle(525,200,100);
-
-// for (let i = 0; i<10; i++){
-//   // fill(0,0,i * spacing)
-//   // circle(spacing + spacing*50,200,40);
-//   fill(colorValue%255);
-//   circle(spacing + i*spacing,200,40);
-//   colorValue += 1;
-
-
-  // Task 1: Colour Gradient
-
-  // Task 2: Colour Loop
-for (let i = 0; i<10; i++){
-  if(i%2 === 0){
-    fill(0);
-  }
-  else {
-    fill(255);
-  }
+    //no loop();
 }
-  circle(spacing + i*spacing,200,40);
-  rect(spacing + i*spacing,250,50,50);
 
+function draw() {
+    // fill(0, 0, 255);
+    // circle(75, 200, 100);
+    // circle(225, 200, 100);
+    // circle(375, 200, 100);
+    // circle(525, 200, 100);
 
-
-  // Task 3: Row of Circles
-
-  // Task 4: Grid of Circles
-
-
+    for(let i = 0; i<5; i++) {
+        //fill(i*50); //fill(r, g, b)
+        //fill(0,0,i*spacing);
+        // fill(colorValue%255);
+        if(i%2 === 0) {
+            fill(0);
+        } else {
+            fill(255);
+        }
+        circle(spacing + i*spacing, 200, 40);
+        rect(spacing + i*spacing, 250, 50, 50);
+        
+        // colorValue +=1; // colorValue = colorValue + 1;
+    }
 }
