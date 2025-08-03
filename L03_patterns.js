ce65e1caafa19ef729c590ae70a53d7768f9ce0d
Computhink-CS128 <cs128@Computhink.com.sg>
2025-08-03 03:40:35 +0000

    Aug 3, 2025, 3:40 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -26,7 +26,9 @@ for (let i = 0; i<10; i++){
   // Task 1: Colour Gradient
 
   // Task 2: Colour Loop
-
+for (let i = 0; i<10; i++){
+  
+}
   // Task 3: Row of Circles
 
   // Task 4: Grid of Circles
