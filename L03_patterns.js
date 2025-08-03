58a8a7ff5278c43f2e77ed2b64ce295f9cfced25
Computhink-CS128 <cs128@Computhink.com.sg>
2025-08-03 03:19:43 +0000

    Aug 3, 2025, 3:19 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -12,7 +12,7 @@ function draw() {
 // circle(375,200,100);
 // circle(525,200,100);
 for (let i = 0; i<10; i++){
-  fill(0,0,i * 100)
+  fill(i * 100,i * 100,i * 100)
   circle(50 + i*50,200,40);
 }
   circle(50 + i*50,200,40);
