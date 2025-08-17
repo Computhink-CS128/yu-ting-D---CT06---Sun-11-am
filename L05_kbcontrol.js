6ba5c209faa25c6fc17943bd4c7b7c9b821b5ad7
Computhink-CS128 <cs128@Computhink.com.sg>
2025-08-17 03:23:02 +0000

    Aug 17, 2025, 3:23 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -1,1 +1,17 @@
 // write your codes here
+
+function setup() {
+    createCanvas(600,600);
+    background(220)
+    
+}
+
+function draw() {
+//     for (let i=0 ; i<5;i++){
+//         // fill(0, i*50, 0)
+//         // circle(50 + i*50, 50 +i*50, 50)
+//         fill(shapeColor);
+//         circle(width/2, height/2, 100);
+
+//     }
+} 
