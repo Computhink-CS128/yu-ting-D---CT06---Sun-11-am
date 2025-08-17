30d1dc99748f7f7ad522a5d1953296f28c1401e2
Computhink-CS128 <cs128@Computhink.com.sg>
2025-08-17 03:33:26 +0000

    Aug 17, 2025, 3:33 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -9,15 +9,6 @@ function setup() {
     
 }
 
-function draw() {
-//     for (let i=0 ; i<5;i++){
-//         // fill(0, i*50, 0)
-//         // circle(50 + i*50, 50 +i*50, 50)
-//         fill(shapeColor);
-//         circle(width/2, height/2, 100);
-
-//     }
-} 
 
 function mousePressed() {
     fill(random(255),random(255),random(255));
