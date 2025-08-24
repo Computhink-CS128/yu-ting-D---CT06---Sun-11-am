174147d4047dc64a7463eca3296e380cfca67b51
Computhink-CS128 <cs128@Computhink.com.sg>
2025-08-24 03:18:12 +0000

    Aug 24, 2025, 3:18 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -1,4 +1,4 @@
-
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
 /*
 Task 1: Add the setup() 
 */
