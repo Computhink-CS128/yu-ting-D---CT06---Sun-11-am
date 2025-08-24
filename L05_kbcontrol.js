12590e3698f529c41a4b6f8ba535834ab939dbd0
Computhink-CS128 <cs128@Computhink.com.sg>
2025-08-24 04:22:01 +0000

    Aug 24, 2025, 4:22 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -3,12 +3,12 @@
 // let shapeColor = 'lime';
 // let circleSize = 5;
 
-function setup() {
-    createCanvas(400,400);
-    background(220)
-    noStroke();
+// function setup() {
+//     createCanvas(400,400);
+//     background(220)
+//     noStroke();
     
-}
+// }
 
 
 // function mousePressed() {
