c5a1d86a20169cb2af6968bb531eccddc9177638
Computhink-CS128 <cs128@Computhink.com.sg>
2025-08-24 03:51:35 +0000

    Aug 24, 2025, 3:51 AM

--- Revision.js
+++ Revision.js
@@ -5,24 +5,5 @@ function setup() {
 
 function draw() {
     // rect(600/2 ,400/2 ,50 ,50)
-
-let diameter = 50;
-let numCircles = 5;
-let totalwidth = diameter*numCircles;
-// let startX = (width - totalwidth)/2 + diameter/2;
-// let startY = (height - totalwidth)/2 + diameter/2;
-let startX = diameter/2;
-let startY = diameter/2;
-
-
-for(let j=0; j<numCircles; j++){
-  let y = startY + j*diameter
-
-  for(let i =0; i<numCircles ; i++ ){
-    circle(startX + i*diameter , y, diameter);
-
-  }
-
-
-}
+    
 }
