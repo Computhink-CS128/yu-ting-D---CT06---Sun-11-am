442c9b4201f673d92a86aae04869407158e3ff7b
Computhink-CS128 <cs128@Computhink.com.sg>
2025-08-24 03:59:41 +0000

    Aug 24, 2025, 3:59 AM

--- Revision.js
+++ Revision.js
@@ -4,7 +4,7 @@ function setup() {
 }
 
 function draw() {
-    // rect(width/2 - size/2 ,/2 ,50 ,50)
+    // rect(width/2 - size/2 ,rect(width/2 - size/2 ,50 ,50)
 
     for(let i; i>5; i++){
         fill
