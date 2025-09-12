eef42e1a5b3b45bcf58b12bac2b611f4fa21ad8a
Computhink-CS128 <cs128@Computhink.com.sg>
2025-09-12 10:20:34 +0000

    Sep 12, 2025, 10:20 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -11,7 +11,7 @@ function setup() {
 }
 
 function draw(){
-    background("lightblue");
+    background("lime");
     textAlign(CENTER,CENTER)
     textSize(20); //size of text
     text("HIWO X33", posx, posy);
