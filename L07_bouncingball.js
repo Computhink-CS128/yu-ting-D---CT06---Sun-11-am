9bb1fcd45d9d176f44330087feaf3837db33683d
Computhink-CS128 <cs128@Computhink.com.sg>
2025-09-14 03:11:38 +0000

    Sep 14, 2025, 3:11 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -21,11 +21,11 @@ function draw(){
         x -= 2;
     }
         if (keyIsDown(UP_ARROW)){
-        y += 2;
+        y -= 2;
     }
 
     if (keyIsDown(DOWN_ARROW)){
-        y -= 2;
+        y += 2;
     }
 
     circle(x,y,50);
