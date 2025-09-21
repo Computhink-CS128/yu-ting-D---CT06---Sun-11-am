1ff2106a2d3824f853027e7ad3e0a6d6754414cb
Computhink-CS128 <cs128@Computhink.com.sg>
2025-09-21 03:57:53 +0000

    Sep 21, 2025, 3:57 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -1,42 +1,42 @@
-// write your codes here
-let Xspeed = 3;
-let posx, posy;
-//sound :DD
-let pic;
-function preload(){
-    bgsound = loadSound("assets/bossaNova.mp3"); //sound
-    pic = loadImage("assets/pico-a.png"); //image
-}
-//sound :DD
+// // write your codes here
+// let Xspeed = 3;
+// let posx, posy;
+// //sound :DD
+// let pic;
+// function preload(){
+//     bgsound = loadSound("assets/bossaNova.mp3"); //sound
+//     pic = loadImage("assets/pico-a.png"); //image
+// }
+// //sound :DD
 
-function setup() {
-    createCanvas(600 , 400);
-    background(220);
+// function setup() {
+//     createCanvas(600 , 400);
+//     background(220);
 
-     posx = width/2; //starting value
-    posy = height/2; //starting value
-}
+//      posx = width/2; //starting value
+//     posy = height/2; //starting value
+// }
 
-function draw(){
-    background("lime");
-    textAlign(CENTER,CENTER)
-    textSize(20); //size of text
-    text("HIWO X33", posx, posy);
-    // image
-    image(pic,50,50,110,133); //draw pico 
-    // image
-    // changes the value of posx
-    posx = posx + Xspeed
+// function draw(){
+//     background("lime");
+//     textAlign(CENTER,CENTER)
+//     textSize(20); //size of text
+//     text("HIWO X33", posx, posy);
+//     // image
+//     image(pic,50,50,110,133); //draw pico 
+//     // image
+//     // changes the value of posx
+//     posx = posx + Xspeed
 
-    //go off the screen and come back the other side
-    // if (posx>width){
-        //posx = -50;
-            // }
+//     //go off the screen and come back the other side
+//     // if (posx>width){
+//         //posx = -50;
+//             // }
 
-}
+// }
 
-function mousePressed(){
-    bgsound.play();
-}
+// function mousePressed(){
+//     bgsound.play();
+// }
 
     
