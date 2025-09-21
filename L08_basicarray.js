8c426750c985661ce3b26ff678b1919500d47179
Computhink-CS128 <cs128@Computhink.com.sg>
2025-09-21 04:04:49 +0000

    Sep 21, 2025, 4:04 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -49,11 +49,12 @@ function setup(){
 }
 
 function draw() {
-    square(x,y,size)
+    square(x,y,size);
+    
     background(255);
         if(x-size/2<=0 || x+size/2>=width){
         speedx *=-1;
         fill(random(255),random(255),random(255))
     }
-
+noStroke();
 }
