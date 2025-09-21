59e2c8398aa8aa0493fc6859335fa4f91b34cb04
Computhink-CS128 <cs128@Computhink.com.sg>
2025-09-21 03:53:39 +0000

    Sep 21, 2025, 3:53 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -99,7 +99,7 @@ function draw() {
 
     function keyPressed() {
         if (keyCode === UP_ARROW) {
-            speedx *= 1.2;
+            speedx *= 2;
             speedy *= 1.2;
 
         } else if (keyCode === DOWN_ARROW){
