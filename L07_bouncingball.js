b75dbe655a0af805a3b7a9db676e965a9bfd82c0
Computhink-CS128 <cs128@Computhink.com.sg>
2025-09-21 03:20:07 +0000

    Sep 21, 2025, 3:20 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -96,7 +96,7 @@ function setup(){
 function draw() {
     background(220)
     if(x-size/2<=0 || x+size/2>=width){
-        speedx *=-1;
+        speedx *=1.2;
         fill(random(255),random(255),random(255))
     }
     if(y-size/2<=0 || y+size/2>=height){
