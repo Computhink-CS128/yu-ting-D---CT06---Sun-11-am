ef464e4c7140b55db33d1d9c07dc53a9ecc950e8
Computhink-CS128 <cs128@Computhink.com.sg>
2025-09-28 03:54:39 +0000

    Sep 28, 2025, 3:54 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -1,2 +1,19 @@
 // write your codes here
-let st
+let 
+function setup() {
+    createCanvas(600,600);
+
+    fill(255,255,0);
+    textSize(24);
+    textAlign(CENTER);
+    ypos = height;
+}
+
+function draw(){
+    background(0);
+
+    for (let i=0; i<favFood.length; i++) {
+        text((i+1) + '.' + favFood[i], width/2,ypos + i*50);
+    }
+    ypos -= 4;
+}
