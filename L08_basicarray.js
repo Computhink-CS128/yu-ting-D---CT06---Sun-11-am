a2a644c0f770b9f66a1d36575d9f4bc9f81185e8
Computhink-CS128 <cs128@Computhink.com.sg>
2025-09-28 03:32:34 +0000

    Sep 28, 2025, 3:32 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -88,6 +88,6 @@ function draw(){
     text("my fav food is: ", width/2, 50);
 
     for (let i=0; i<favFood.length; i++) {
-        text((i+1) + '.' + favFood[1], width/2, 80 + i*50);
+        text((i+1) + '.' + favFood[i], width/2, 80 + i*50);
     }
 }
