ac166584f02220ce9c525704aa9ccb9a9325dadf
Computhink-CS128 <cs128@Computhink.com.sg>
2025-09-28 04:17:51 +0000

    Sep 28, 2025, 4:17 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -9,31 +9,31 @@
 // }
 // //sound :DD
 
-// function setup() {
-//     createCanvas(600 , 400);
-//     background(220);
+function setup() {
+    createCanvas(600 , 400);
+    background(220);
 
-//      posx = width/2; //starting value
-//     posy = height/2; //starting value
-// }
+     posx = width/2; //starting value
+    posy = height/2; //starting value
+}
 
-// function draw(){
-//     background("lime");
-//     textAlign(CENTER,CENTER)
-//     textSize(20); //size of text
-//     text("HIWO X33", posx, posy);
-//     // image
-//     image(pic,50,50,110,133); //draw pico 
-//     // image
-//     // changes the value of posx
-//     posx = posx + Xspeed
+function draw(){
+    background("lime");
+    textAlign(CENTER,CENTER)
+    textSize(20); //size of text
+    text("HIWO X33", posx, posy);
+    // image
+    image(pic,50,50,110,133); //draw pico 
+    // image
+    // changes the value of posx
+    posx = posx + Xspeed
 
-//     //go off the screen and come back the other side
-//     // if (posx>width){
-//         //posx = -50;
-//             // }
+    //go off the screen and come back the other side
+    // if (posx>width){
+        //posx = -50;
+            // }
 
-// }
+}
 
 // function mousePressed(){
 //     bgsound.play();
