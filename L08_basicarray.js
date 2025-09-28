11e5255373990845aa1cf40dd405c04670b806cc
Computhink-CS128 <cs128@Computhink.com.sg>
2025-09-28 03:51:09 +0000

    Sep 28, 2025, 3:51 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -92,8 +92,29 @@
 //     }
 // }
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// let ypos;
+// let favFood = ['Pizza','Burger','Sushi',"Tacos","Pasta","wsp gng","hiwo","im bored","i lov forsaken >:3","they say my hungers a problem"];
+
+// function setup() {
+//     createCanvas(600,600);
+
+//     fill(255,255,0);
+//     textSize(24);
+//     textAlign(CENTER);
+//     ypos = height;
+// }
+
+// function draw(){
+//     background(0);
+
+//     for (let i=0; i<favFood.length; i++) {
+//         text((i+1) + '.' + favFood[i], width/2,ypos + i*50);
+//     }
+//     ypos -= 4;
+// }
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 let ypos;
-let favFood = ['Pizza','Burger','Sushi',"Tacos","Pasta","wsp gng","hiwo","im bored","i lov forsaken >:3","they say my hungers a problem"];
+let sillyText = ["wsp gng","hiwo","im bored","i lov forsaken >:3","they say my hungers a problem"];
 
 function setup() {
     createCanvas(600,600);
