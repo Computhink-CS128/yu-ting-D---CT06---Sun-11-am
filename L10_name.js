57a6c6cf404e868a3ef53ae49a3931c04188ffb8
Computhink-CS128 <cs128@Computhink.com.sg>
2025-10-05 04:19:10 +0000

    Oct 5, 2025, 4:19 AM

--- L10_name.js
+++ L10_name.js
@@ -6,13 +6,13 @@ let bgColor = 220;
 function setup() {
     createCanvas(400,400);
     textAlign(CENTER,CENTER);
-    
+
     interval = setInterval(countdown,1000);
 }
 function draw() {
     background(bgColor)
 
-    let s = second();
+    let s = 59-second();
     textSize(50);
     text(s,width/2,height/2);
 
