9009eeb0ef704d2db27770a427f00de5f617b37e
Computhink-CS128 <cs128@Computhink.com.sg>
2025-10-05 03:54:26 +0000

    Oct 5, 2025, 3:54 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -80,9 +80,6 @@ let bg = 220;
 
 function setup() {
     createCanvas(600,400);
-        function preload(){
-            bgsound = loadSound("assets/bossaNova.mp3"); //sound
-  }
 }
 
 function draw() {
