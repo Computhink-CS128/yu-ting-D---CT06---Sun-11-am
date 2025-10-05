1fc2aac4b36cedf348de8526e056b1e0d2aa1eae
Computhink-CS128 <cs128@Computhink.com.sg>
2025-10-05 03:11:29 +0000

    Oct 5, 2025, 3:11 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -1,23 +1,24 @@
-// write your codes here
-let Xspeed = 3;
-let posx, posy;
-function setup() {
-    createCanvas(600 , 400);
-    background(220);
+// // write your codes here
+// let Xspeed = 3;
+// let posx, posy;
+// function setup() {
+//     createCanvas(600 , 400);
+//     background(220);
 
-textSize(22)
+// textSize(22)
 
-}
+// }
 
-function draw(){
+// function draw(){
 
-    text("HIWO X33", posx, posy);
+//     text("HIWO X33", posx, posy);
  
-    // changes the value of posx
-    posx -= Xspeed
+//     // changes the value of posx
+//     posx -= Xspeed
 
-    if(x<=0){
-        Xspeed *=-1;
-    }
+//     if(x<=0){
+//         Xspeed *=-1;
+//     }
 
-}
+// }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
