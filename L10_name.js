7be95ddaf51832d6f66e9dac947f7e1c9932ea7c
Computhink-CS128 <cs128@Computhink.com.sg>
2025-10-05 04:15:31 +0000

    Oct 5, 2025, 4:15 AM

--- L10_name.js
+++ L10_name.js
@@ -4,7 +4,7 @@
 function setup() {
     createCanvas(400,400);
     textAlign(CENTER,CENTER);
-    interval = setInterval(countdow)
+    interval = setInterval(countdown,1000)
 }
 function draw() {
     background(220)
