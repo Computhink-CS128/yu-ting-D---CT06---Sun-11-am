bc34b1e52f30a8c34f109306a550d6197c0e1a1f
Computhink-CS128 <cs128@Computhink.com.sg>
2025-10-05 03:29:55 +0000

    Oct 5, 2025, 3:29 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -23,24 +23,24 @@
 // }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-function setup() {
-    createCanvas(600,400);
-}
+// function setup() {
+//     createCanvas(600,400);
+// }
 
-function draw() {
-    background(220);
-    textAlign(CENTER,CENTER)
+// function draw() {
+//     background(220);
+//     textAlign(CENTER,CENTER)
 
-    let h = hour();
-    let m = minute();
-    let s = second();
+//     let h = hour();
+//     let m = minute();
+//     let s = second();
 
-    let timeString = nf(h,2) + ':' + nf(m,2) +':' + nf(s,2);
-    textSize(72);
-    fill('lime')
-    text(timeString, width/2, height/2)
+//     let timeString = nf(h,2) + ':' + nf(m,2) +':' + nf(s,2);
+//     textSize(72);
+//     fill('lime')
+//     text(timeString, width/2, height/2)
 
-}
+// }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 function setup() {
     createCanvas(600,400);
