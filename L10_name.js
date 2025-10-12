3b8c46672de2b7a7a15fffcf527a7a76432301d2
Computhink-CS128 <cs128@Computhink.com.sg>
2025-10-12 04:34:06 +0000

    Co-authored-by: Lyn101s <Lyn101s@users.noreply.github.com>

--- L10_name.js
+++ L10_name.js
@@ -22,26 +22,128 @@
 //     bgColor= color(random(255),random(255),random(255));
 
 // }
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-let imputText;
-let userText = 'defalt';
+// let imputText;
+// let userText = "text shown here";
+
+// function setup() {
+//     createCanvas(600,400);
+
+//     imputText = createInput();
+//     imputText.position(width/2,height-100);
+//     imputText.input(updateText);
+//     imputText.size(150,50);
+// }
+
+// function draw() {
+//     background(220);
+//     text('enter text here',50,height-80);
+//     text(userText,width/2,height/2);
+// }
+
+// function updateText() {
+//     userText = this.value();
+// }
+// //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// let colorPicker;
+
+// function setup(){
+//     createCanvas(600,400);
+
+//     colorPicker = createColorPicker('orange');
+//     colorPicker.position(width/2-50,height-50);
+// }
+
+// function draw(){
+//     background(colorPicker.value());
+
+//     textAlign(LEFT,BOTTOM);
+//     text("Pick A Colour Background XD:",50 ,colorPicker.y+20);
+
+//     rect(50,100,500,160,50)
+
+// }
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// let imputText;
+// let userText = "text shown here";
 
 function setup() {
     createCanvas(600,400);
 
-    inputText = createInput();
-    inputText.position(width/2,height-100);
-    inputText.input(updateText);
-    inputText.size(150,50);
+    // imputText = createInput();
+    // imputText.position(width/2,height-100);
+    // imputText.input(updateText);
+    // imputText.size(150,50);
 }
 
-function draw() {
-    background(220);
-    Text('enter text here',50,height-80);
-    Text(userText,width/2,height/2);
+// function draw() {
+//     // background(220);
+//     // text('enter text here',50,height-80);
+//     // text(userText,width/2,height/2);
+// }
+
+// function updateText() {
+//     userText = this.value();
+// }
+
+let colorPicker;
+let colorPickerText;
+let imputText;
+let userTextName = "your name";
+let inputAge;
+let userTextAge = "your age"
+
+function setup(){
+    textAlign(CENTER,CENTER)
+    createCanvas(600,400);
+
+    colorPicker= createColorPicker('orange');
+    colorPicker.position(width/2-50,height-30);
+
+    colorPickerText = createColorPicker('pink');
+    colorPickerText.position(width/2-50,height-70);
+
+    imputText = createInput();
+    imputText.position(width/2-50,height-90);
+    imputText.input(updateText);
+    imputText.size(150,10);
+
+    imputAge = createInput();
+    imputAge.position(width/2-50,height-115);
+    imputAge.input(updateTextAge);
+    imputAge.size(150,10);
 }
 
-function update() {
-    userText = this.value();
+function draw(){
+    background(colorPicker.value());
+
+    textAlign(LEFT,BOTTOM);
+    text("Pick A Background Colour XD:",50 ,colorPicker.y+20);
+
+    textAlign(LEFT,BOTTOM);
+    text("Pick A Text Colour XD:",50 ,colorPickerText.y+30);
+
+    fill(255,255,255)
+    rect(50,100,500,160,50)
+
+    textAlign(CENTER,CENTER)
+    fill(colorPickerText.value())
+    text('enter name',200,height-80);
+    text(userTextName,width/2,height/2-30);
+
+    fill(colorPickerText.value())
+    textAlign(CENTER,CENTER)
+    text('enter age',200,height-100);
+    text(userTextAge,width/2,height/2);
+
+}
+
+function updateText() {
+    userTextName = this.value();
+}
+function updateTextAge() {
+    userTextAge = this.value();
 }
---
+++ revison2.js
@@ -0,0 +1,37 @@
+// let arr = ["pizza","maccoron","pies","chocolate"];
+
+// function setup() {
+//     createCanvas(600 , 400);
+//     background(0);
+// }
+
+// function draw() {
+//     fill("orange");
+//     textAlign(CENTER,CENTER);
+//     textSize(24);
+
+//     for (let i=0; i<arr.length; i++) {
+//     text(arr[i], width/2, 100 + i*50);
+//     }
+// }
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function setup(){
+    createCanvas(600 , 400);
+}
+
+let x = 600/2
+let y = 400/2
+let xSpeed = 5
+
+function draw() {
+    background("lightblue");
+
+    if(x-50/2<=0 || x+50/2>600){
+        xSpeed *=-1;
+    }
+    x +=xSpeed;
+
+    fill(225,0,0)
+    noStroke();
+    circle(x,y,50);
+}
