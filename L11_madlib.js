856898e476eaa580d190fa70c7a5d20ce93884b5
Computhink-CS128 <cs128@Computhink.com.sg>
2025-10-19 03:36:57 +0000

    Oct 19, 2025, 3:36 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -1,32 +1,32 @@
 // write your codes here
 
-let colorPicker;
-let textBox;
-let display;
-let textColourPicker;
+// let colorPicker;
+// let textBox;
+// let display;
+// let textColourPicker;
 
-function setup(){
-    createCanvas(400,400);
+// function setup(){
+//     createCanvas(400,400);
 
-    textBox = createInput();
-    textBox.position(50,50);
-    textBox.size(150,30);
-    textBox.input(displayText);
+//     textBox = createInput();
+//     textBox.position(50,50);
+//     textBox.size(150,30);
+//     textBox.input(displayText);
 
-    colorPicker= createColorPicker('orange');
-    colorPicker.position(50,100);
-}
+//     colorPicker= createColorPicker('orange');
+//     colorPicker.position(50, 100);
+// }
 
-function draw(){
-    background(220);
+// function draw(){
+//     background(220);
 
-    fill(colorPicker.value())
-    textAlign(LEFT);
-    textSize(24);
-    text(display,50,height/2);
+//     fill(colorPicker.value())
+//     textAlign(LEFT);
+//     textSize(24);
+//     text(display,50,height/2);
 
-}
+// }
 
-function displayText(){
-    display = this.value();
-}
+// function displayText(){
+//     display = this.value();
+// }
