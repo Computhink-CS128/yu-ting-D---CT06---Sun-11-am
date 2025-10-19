f1255600b6ce6904a746d97a09052a83162cf410
Computhink-CS128 <cs128@Computhink.com.sg>
2025-10-19 03:41:49 +0000

    Oct 19, 2025, 3:41 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -42,7 +42,7 @@ function setup(){
     textBox.position(100,50);
 
     button= createButton('bite o 87');
-    button.position(textBox)
+    button.position(textBox.x+textBox)
 }
 
 function draw(){
