ea9a7ec481953a0342588cfcc42e26c7b9183f56
Computhink-CS128 <cs128@Computhink.com.sg>
2025-10-19 03:59:15 +0000

    Oct 19, 2025, 3:59 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -65,7 +65,7 @@ let textBox1,textBox2,textBox3,textBox4,textBox5;
 function setup(){
 
     textBox1 = createInput();
-    textBox1.position(width)
+    textBox1.position(width/2-textBox1)
 }
 function draw(){
 
