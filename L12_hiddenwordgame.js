05fffacbf7cb937a1fd0a97d08488ad20bd78f88
Computhink-CS128 <cs128@Computhink.com.sg>
2025-11-02 04:16:50 +0000

    Nov 2, 2025, 4:16 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -5,3 +5,5 @@ let words = [
     "football", "bathroom", "sandwich", "airplane", "umbrella", "medicine", "chocolate", "software", 
     "pineapple", "furniture", "telephone", "lighthouse"];
 
+
+    fucnti
