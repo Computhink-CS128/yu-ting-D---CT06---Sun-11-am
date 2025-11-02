3dcc0fc88f36db91f5884dd5ec36796fe69d7828
Computhink-CS128 <cs128@Computhink.com.sg>
2025-11-02 04:17:51 +0000

    Nov 2, 2025, 4:17 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -4,7 +4,7 @@ let words = [
     "elephant", "backpack", "keyboard", "hospital", "sunlight", "raincoat", "notebook", "shoulder", 
     "football", "bathroom", "sandwich", "airplane", "umbrella", "medicine", "chocolate", "software", 
     "pineapple", "furniture", "telephone", "lighthouse"];
-
+let attempts =0;
 
     function setup() {
         createCanvas(600,400);
