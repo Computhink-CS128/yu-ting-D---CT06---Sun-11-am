188db6c4d202fbad7dcdf2e0ef667f744df813f7
Computhink-CS128 <cs128@Computhink.com.sg>
2025-11-09 04:08:02 +0000

    Nov 9, 2025, 4:08 AM

--- L13_wordscramblegame.js
+++ L13_wordscramblegame.js
@@ -33,7 +33,7 @@ function draw() {
     textAlign(CENTER, CENTER); 
     textSize(24); 
     text("Guess za Word >:3", width/2, 50); 
-    text("score: " + attempts, width/2, 100); 
+    text("score: " + score, width/2, 100); 
     text("Hint: " + hiddenWord, width/2, 150); 
     text(message, width/2, 250); 
 
