da8e7c56fcdf8939e0353f34fcd15e0fae466efc
Computhink-CS128 <cs128@Computhink.com.sg>
2025-11-09 03:49:03 +0000

    Nov 9, 2025, 3:49 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -52,7 +52,7 @@ let message;
         //     message= "FIVE LETTERS ONLY FOR STARCLANS SAKE >:("
         } 
         else {
-            message = "Try again :D"
+            message = "TrOH FOR STAR"
         }
 
     }
