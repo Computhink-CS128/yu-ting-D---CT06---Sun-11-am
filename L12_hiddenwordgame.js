f35efeed5b566afece86514d5c233124dce37a24
Computhink-CS128 <cs128@Computhink.com.sg>
2025-11-09 04:01:49 +0000

    Nov 9, 2025, 4:01 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -122,7 +122,7 @@ function checkGuess() {
     message = getCorrectLetters(guess, randWord); 
 
     if(guess == randWord) {
-        message = " " + randWord; 
+        message = "wow star " + randWord; 
     } else {
         message = "Try again. The letters you get it right is/are: " + message; 
     }
